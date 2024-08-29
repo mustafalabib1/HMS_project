9a5f68b91489def60eeaccd807585d3b7751a709
Mustafa Labib <mostafalabib2004@gmail.com>
2024-08-30 00:22:47 +0300

    update common classes

---
+++ HMS_Project/common/Pharmacist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common
+{
+    public class Pharmacist
+    {
+        private int pharmacistID;
+        public int PharmacistID
+        {
+            get { return pharmacistID; }
+            set { pharmacistID = value; }
+        }
+
+        private int pharmacyId;
+        public int PharmacyId
+        {
+            get { return pharmacyId; }
+            set { pharmacyId = value; }
+        }
+
+        private long ssn;
+        public long SSN
+        {
+            get { return ssn; }
+            set { ssn = value; }
+        }
+    }
+}
--- HMS_Project/common/obj/Debug/net6.0/common.AssemblyInfo.cs
+++ HMS_Project/common/obj/Debug/net6.0/common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e889e8f1a9ac1d5c5d2601d18a4098dbaa4586a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
