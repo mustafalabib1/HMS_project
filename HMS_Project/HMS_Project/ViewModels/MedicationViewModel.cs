9167af7ba056a237c24364227b5629efd26a0533
AhmedAli <ahmed.khalil@std.mans.edu.eg>
2024-09-29 23:30:17 +0300

    Add model view (PrescriptionItem and Madication)

---
+++ HMS_Project/HMS_Project/ViewModels/MedicationViewModel.cs
@@ -0,0 +1,50 @@
+﻿using DALProject.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS_Project.ViewModels
+{
+    internal class MedicationViewModel
+    {
+        public MedicationViewModel() { }
+
+        public MedicationViewModel(Medication medication)
+        {
+            MedicationCode = medication.MedicationCode;
+            MedName = medication.MedName;
+            Strength = medication.Strength;
+        }
+
+        [Required]
+        public string MedicationCode { get; set; } = null!;
+
+        [Required]
+        public string MedName { get; set; } = null!;
+
+        [Required]
+        public int Strength { get; set; }
+
+        
+        public static explicit operator Medication(MedicationViewModel medViewModel)
+        {
+            return new Medication()
+            {
+                MedicationCode = medViewModel.MedicationCode,
+                MedName = medViewModel.MedName,
+                Strength = medViewModel.Strength
+            };
+        }
+
+        
+        public static explicit operator MedicationViewModel(Medication medication)
+        {
+            return new MedicationViewModel(medication);
+        }
+
+
+    }
+}
--- HMS_Project/HMS_Project/obj/Debug/net8.0/HMS_Project.AssemblyInfo.cs
+++ HMS_Project/HMS_Project/obj/Debug/net8.0/HMS_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ce4d353760d5c087428b466c0fe4ce081a013a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7fdd2c149fd8c64e9814fb847e4c147577760a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMS_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
