46253f850fb4fcf236f50c2fb1f3d7b571ee454e
MohamedHawy <mohamedhawy2003@gmail.com>
2024-10-04 21:41:12 +0300

    Edit
    Index for Invoice

--- HMS_Project/PLProject/Controllers/InvoiceController.cs
+++ HMS_Project/PLProject/Controllers/InvoiceController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLLProject.Interfaces;
+using DALProject.model;
+using HMS_Project.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PLProject.Controllers
 {
     public class InvoiceController : Controller
     {
+        private readonly IRepository<Invoice> repository;
+
+        public InvoiceController(IRepository<Invoice> repository)
+        {
+            this.repository = repository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var Invoices = repository.GetALL();
+            var InvoiceViewModel = Invoices.Select(c => (InvoiceViewModel)c).ToList();
+            return View(Invoices);
         }
     }
 }
