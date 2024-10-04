6a446012ce67e7350fe27ba0a982dd4442708093
Fares Mahmoud <yepiwater@outlook.com>
2024-10-04 19:37:00 +0300

    Made Details, Edit and Delete for Clinic

--- HMS_Project/PLProject/Controllers/ClinicController.cs
+++ HMS_Project/PLProject/Controllers/ClinicController.cs
@@ -6,18 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PLProject.Controllers
 {
-	public class ClinicController : Controller
-	{
-		#region DPI
-		private readonly IRepository<Clinic> clinicRepo;
+    public class ClinicController : Controller
+    {
+        #region DPI
+        private readonly IRepository<Clinic> clinicRepo;
 
-		public IRepository<ClinicSpecializationLookup> SpecializationRepo { get; }
+        public IRepository<ClinicSpecializationLookup> SpecializationRepo { get; }
 
-		public ClinicController(IRepository<ClinicSpecializationLookup> SpecializationRepo, IRepository<Clinic> ClinicRepo)
-		{
-			this.SpecializationRepo = SpecializationRepo;
-			clinicRepo = ClinicRepo;
-		}
+        public ClinicController(IRepository<ClinicSpecializationLookup> SpecializationRepo, IRepository<Clinic> ClinicRepo)
+        {
+            this.SpecializationRepo = SpecializationRepo;
+            clinicRepo = ClinicRepo;
+        }
         #endregion
         public IActionResult Index()
         {
@@ -26,45 +26,134 @@ namespace PLProject.Controllers
             return View(clinicViewModels);
         }
 
-		#region Specialization
-		public IActionResult AddSpecialization()
-		{
-			return View();
-		}
+        #region Specialization
+        public IActionResult AddSpecialization()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public IActionResult AddSpecialization(ClinicSpecializationLookup clinicSpecializationLookup)
-		{
-			if (ModelState.IsValid) // server side validation
-			{
-				SpecializationRepo.Add(clinicSpecializationLookup);
-				return RedirectToAction(nameof(Index));
-			}
-			return View(clinicSpecializationLookup);
-		} 
-		#endregion
+        [HttpPost]
+        public IActionResult AddSpecialization(ClinicSpecializationLookup clinicSpecializationLookup)
+        {
+            if (ModelState.IsValid) // server side validation
+            {
+                SpecializationRepo.Add(clinicSpecializationLookup);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(clinicSpecializationLookup);
+        }
+        #endregion
 
-		#region Create
-		public IActionResult Create()
-		{
-			var ViewModel = new ClinicViewModel() { SpecializationsDateReader = SpecializationRepo.GetALL() };
+        #region Create
+        public IActionResult Create()
+        {
+            var ViewModel = new ClinicViewModel() { SpecializationsDateReader = SpecializationRepo.GetALL() };
 
-			return View(ViewModel);
-		}
+            return View(ViewModel);
+        }
 
-		[HttpPost]
-		public IActionResult Create(ClinicViewModel clinicViewModel)
-		{
-			if (ModelState.IsValid) // server side validation
-			{
-				clinicRepo.Add((Clinic)clinicViewModel);
-				return RedirectToAction(nameof(Index));
-			}
-			clinicViewModel.SpecializationsDateReader = SpecializationRepo.GetALL();
-			return View(clinicViewModel);
-		}
-		#endregion
+        [HttpPost]
+        public IActionResult Create(ClinicViewModel clinicViewModel)
+        {
+            if (ModelState.IsValid) // server side validation
+            {
+                clinicRepo.Add((Clinic)clinicViewModel);
+                return RedirectToAction(nameof(Index));
+            }
+            clinicViewModel.SpecializationsDateReader = SpecializationRepo.GetALL();
+            return View(clinicViewModel);
+        }
+        #endregion
 
+        #region Details
+        public IActionResult Details(int? Id)
+        {
+            if (!Id.HasValue)
+                return BadRequest(); // 400
 
-	}
+            var clinic = clinicRepo.Get(Id.Value);
+            var clinicViewModel = (ClinicViewModel)clinic;
+
+            if (clinic is null)
+                return NotFound(); // 404
+
+            return View(clinicViewModel);
+        }
+
+        #endregion
+
+        #region Edit
+
+        public IActionResult Edit(int? Id)
+        {
+            if (!Id.HasValue)
+                return BadRequest(); // 400
+
+            var clinic = clinicRepo.Get(Id.Value);
+
+            if (clinic is null)
+                return NotFound(); // 404
+
+            var clinicViewModel = (ClinicViewModel)clinic;
+
+            clinicViewModel.SpecializationsDateReader = SpecializationRepo.GetALL();
+            return View(clinicViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ClinicViewModel clinicViewModel)
+        {
+            Clinic clinic = clinicRepo.Get(clinicViewModel.Id);
+
+            if (ModelState.IsValid) // server side validation
+            {
+                // Mapping the model
+                clinic.Name = clinicViewModel.Name;
+                clinic.Specialization = clinicViewModel.Specialization;
+                clinic.Phone = clinicViewModel.Phone;
+                clinic.Price = clinicViewModel.Price ?? default;
+
+                clinicRepo.Update(clinic);
+                return RedirectToAction(nameof(Index));
+            }
+            clinicViewModel.SpecializationsDateReader = SpecializationRepo.GetALL();
+            return View(clinicViewModel);
+        }
+
+        #endregion
+
+        #region Delete
+
+        public IActionResult Delete(int? Id)
+        {
+            if (!Id.HasValue)
+                return BadRequest(); // 400
+
+            var clinic = clinicRepo.Get(Id.Value);
+            var clinicViewModel = (ClinicViewModel)clinic;
+
+            if (clinic is null)
+                return NotFound(); // 404
+
+            return View(clinicViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ClinicViewModel clinicViewModel)
+        {
+            var clinic = clinicRepo.Get(clinicViewModel.Id);
+            try
+            {
+                clinicRepo.Delete(clinic);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(clinic);
+            }
+        }
+        #endregion
+
+    }
 }
