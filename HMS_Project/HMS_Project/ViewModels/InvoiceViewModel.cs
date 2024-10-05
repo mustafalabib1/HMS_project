b935a8d303536bfe1ea0ab0d2f3db0df2b49e22c
MohamedHawy <mohamedhawy2003@gmail.com>
2024-10-05 17:07:33 +0300

    Final Edition
    All Pages For Invoice

--- HMS_Project/HMS_Project/ViewModels/InvoiceViewModel.cs
+++ HMS_Project/HMS_Project/ViewModels/InvoiceViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using DALProject.model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,17 +18,38 @@ namespace HMS_Project.ViewModels
             TotalAmount = invoice.TotalAmount;
             PaymentStatus = invoice.PaymentStatus;
             PaymentType= Enum.TryParse(invoice.PaymentType, out PaymentType paymentType) ? paymentType : null;
+            ReceptionistId = invoice.ReceptionistId;
+            ApointmentId = invoice.ApointmentId;
         }
 
         public int InvoiceID { get; set; }
+
+        [Display(Name = "Invoice Date")]
+        [Required(ErrorMessage = " Please Choose a Date.")]
         public DateTime InvoiceDate { get; set; }
+
+        [Display(Name = "Total Amount")]
+        [Required(ErrorMessage = " Please Enter Total Amount.")]
         public decimal TotalAmount { get; set; }
+        [Display(Name = "Payment Status")]
+        [Required(ErrorMessage = " Please Select Status.")]
         public bool PaymentStatus { get; set; }
+
+        [Display(Name = "Payment Type")]
+        [Required(ErrorMessage = " Please Select Type.")]
         public PaymentType? PaymentType { get; set; }
 
         public IEnumerable <Receptionist> ReceptionistsReader { get; set; } = new HashSet<Receptionist>();
+
+        [Display(Name = "Receptionist Name")]
+        [Required(ErrorMessage = " Please Select Receptionist Name.")]
+        public long? ReceptionistId { get; set; }
         public IEnumerable<Apointment>  ApointmentsReader { get; set; } = new HashSet<Apointment>();
-       
+        [Display(Name = "Appointment Date")]
+        [Required(ErrorMessage = " Please Select Receptionist Name.")]
+        public int? ApointmentId { get; set; }
+
+
 
         public static explicit operator Invoice(InvoiceViewModel invoiceViewModel)
         {
--- HMS_Project/PLProject/Controllers/InvoiceController.cs
+++ HMS_Project/PLProject/Controllers/InvoiceController.cs
@@ -8,16 +8,127 @@ namespace PLProject.Controllers
     public class InvoiceController : Controller
     {
         private readonly IRepository<Invoice> repository;
+        private readonly IRepository<Receptionist> recprepository;
+        private readonly IRepository<Apointment> apprepository;
 
-        public InvoiceController(IRepository<Invoice> repository)
+        public InvoiceController(IRepository<Invoice> repository, IRepository<Receptionist> Recprepository, IRepository<Apointment> Apprepository)
         {
             this.repository = repository;
+           recprepository = Recprepository;
+           apprepository = Apprepository;
         }
         public IActionResult Index()
         {
             var Invoices = repository.GetALL();
             var InvoiceViewModel = Invoices.Select(c => (InvoiceViewModel)c).ToList();
             return View(Invoices);
+        }
+        #region Create
+        public IActionResult Create()
+        {
+            var invoiceviewmodel = new InvoiceViewModel()
+            {
+                PaymentType = PaymentType.Cash,
+                ReceptionistsReader = recprepository.GetALL(),
+                ApointmentsReader = apprepository.GetALL(),
+
+            };
+            return View(invoiceviewmodel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ApointmentViewModel model)
+        {
+            return View();
+        } 
+        #endregion
+
+        #region Details
+        public IActionResult Details(int? Id)
+        {
+            if (!Id.HasValue)
+                return BadRequest();
+
+            var invoice = repository.Get(Id.Value);
+            var invoiceViewModel = (InvoiceViewModel)invoice;
+
+            if (invoice is null)
+                return NotFound();
+
+            return View(invoiceViewModel);
+        }
+        #endregion
+
+        #region Edit
+        public IActionResult Edit(int? Id)
+        {
+            if (!Id.HasValue)
+                return BadRequest();
+
+            var invoice = repository.Get(Id.Value);
+
+            if (invoice is null)
+                return NotFound();
+            var invoiceviewmodel = (InvoiceViewModel)invoice;
+
+            invoiceviewmodel.ReceptionistsReader = recprepository.GetALL();
+            invoiceviewmodel.ApointmentsReader = apprepository.GetALL();
+
+            return View(invoiceviewmodel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(InvoiceViewModel invoiceViewModel)
+        {
+            Invoice invoice = repository.Get(invoiceViewModel.InvoiceID);
+
+            if (ModelState.IsValid) // server side validation
+            {
+                // Mapping the model
+                invoice.InvoiceDate = invoiceViewModel.InvoiceDate;
+                invoice.TotalAmount = invoiceViewModel.TotalAmount;
+                invoice.ReceptionistId = invoiceViewModel.ReceptionistId;
+                invoice.ApointmentId = invoiceViewModel.ApointmentId;
+                invoice.PaymentStatus = invoiceViewModel.PaymentStatus;
+                invoice.PaymentType = invoiceViewModel.PaymentType.ToString();
+
+                repository.Update(invoice);
+                return RedirectToAction(nameof(Index));
+            }
+            invoiceViewModel.ReceptionistsReader = recprepository.GetALL();
+            invoiceViewModel.ApointmentsReader = apprepository.GetALL();
+            return View(invoiceViewModel);
+        }
+        #endregion
+
+        public IActionResult Delete(int? Id)
+        {
+            if (!Id.HasValue)
+                return BadRequest(); 
+
+            var invoice = repository.Get(Id.Value);
+            var invoiceviewmodel = (InvoiceViewModel)invoice;
+
+            if (invoice is null)
+                return NotFound();
+
+            return View(invoiceviewmodel);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(InvoiceViewModel invoiceViewModel)
+        {
+            var ivoice = repository.Get(invoiceViewModel.InvoiceID);
+            try
+            {
+                repository.Delete(ivoice);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(ivoice);
+            }
         }
     }
 }
