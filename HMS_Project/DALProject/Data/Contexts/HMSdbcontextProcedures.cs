43e96c80c9f8c41970f63e6f75b1cc5ae40df297
Mustafa Labib <158358554+mustafalabib1@users.noreply.github.com>
2024-10-07 04:22:02 +0300

    Add Delete Active Substance Procedure

--- HMS_Project/DALProject/Data/Contexts/HMSdbcontextProcedures.cs
+++ HMS_Project/DALProject/Data/Contexts/HMSdbcontextProcedures.cs
@@ -43,7 +43,7 @@ namespace DALProject.Data.Contexts
             _context = context;
         }
 
-        public virtual async Task<int> sp_DeleteActiveSubstanceAsync(int? Id, OutputParameter<int> returnValue = null, CancellationToken cancellationToken = default)
+        public virtual async Task<int> sp_DeleteActiveSubstanceAsync(int? ActiveSubstancesId, OutputParameter<int> returnValue = null, CancellationToken cancellationToken = default)
         {
             var parameterreturnValue = new SqlParameter
             {
@@ -57,12 +57,12 @@ namespace DALProject.Data.Contexts
                 new SqlParameter
                 {
                     ParameterName = "Id",
-                    Value = Id ?? Convert.DBNull,
+                    Value = ActiveSubstancesId ?? Convert.DBNull,
                     SqlDbType = System.Data.SqlDbType.Int,
                 },
                 parameterreturnValue,
             };
-            var _ = await _context.Database.ExecuteSqlRawAsync("EXEC @returnValue = [dbo].[sp_DeleteActiveSubstance] @Id = @Id", sqlParameters, cancellationToken);
+            var _ = await _context.Database.ExecuteSqlRawAsync("EXEC @returnValue = [dbo].[sp_DeleteActiveSubstance] @ActiveSubstancesId = @Id", sqlParameters, cancellationToken);
 
             returnValue?.SetValue(parameterreturnValue.Value);
 
