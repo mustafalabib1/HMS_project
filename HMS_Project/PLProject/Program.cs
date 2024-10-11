ec34fbe8d72699459af1695528d8d5c3507d6f0e
Fares Mahmoud <faressafi203@gmail.com>
2024-10-11 05:37:20 +0300

    Added DbInitializer which Updates and Initializes Database on Application Startup

--- HMS_Project/PLProject/Program.cs
+++ HMS_Project/PLProject/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.DependencyInjection;
 using NuGet.Protocol.Core.Types;
 using DALProject.model;
+using DALProject.DbInitializer;
 
 namespace PLProject
 {
@@ -21,17 +22,19 @@ namespace PLProject
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
 
-            // Configure the DbContext 
+            #region DbContext
             builder.Services.AddDbContext<HMSdbcontext>(options =>
             {
                 options
                 .UseLazyLoadingProxies()
                 .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
+            }); 
+            #endregion
 
+            #region Identity
             builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<HMSdbcontext>()
-                .AddDefaultTokenProviders();
+        .AddEntityFrameworkStores<HMSdbcontext>()
+        .AddDefaultTokenProviders();
 
             // Configure the cookie settings used for authentication middleware.
             builder.Services.ConfigureApplicationCookie(options =>
@@ -39,8 +42,10 @@ namespace PLProject
                 options.LoginPath = "/Identity/Account/Login";
                 options.LogoutPath = "/Identity/Account/Logout";
                 options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-            });
+            }); 
+            #endregion
 
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
             builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
             builder.Services.AddScoped<IEmailSender, EmailSender>();
             builder.Services.AddScoped<HMSdbcontextProcedures>();
@@ -69,6 +74,17 @@ namespace PLProject
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            #region Update and Initialize Database
+            // Applies pending migrations also if there are no roles, create the default
+            // admin user with email = admin@hmsproject.com and password = Admin#123
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                dbInitializer.Initialize();
+            }
+            #endregion
 
             app.Run();
         }
