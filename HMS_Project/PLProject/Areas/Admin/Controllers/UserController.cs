327cc33be020eb4168c958a4bd92bca8c84393f0
Fares Mahmoud <yepiwater@outlook.com>
2024-10-13 20:19:14 +0300

    added a pages to view Users and System Roles

--- HMS_Project/PLProject/Areas/Admin/Controllers/UserController.cs
+++ HMS_Project/PLProject/Areas/Admin/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using BLLProject.Interfaces;
 using DALProject.model;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PLProject.Areas.Admin.ViewModels;
 
 namespace PLProject.Areas.Admin.Controllers
 {
@@ -9,16 +12,33 @@ namespace PLProject.Areas.Admin.Controllers
     [Authorize(Roles = Roles.Admin)]
     public class UserController : Controller
     {
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public UserController(IUnitOfWork unitOfWork)
+        public UserController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
         {
-            _unitOfWork = unitOfWork;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var users = await _userManager.Users.Select(user => new AppUserViewModel
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FullName = user.FullName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber ?? string.Empty,
+                Roles = _userManager.GetRolesAsync(user).Result
+            }
+                ).ToListAsync();
+            return View(users);
+        }
+        public async Task<IActionResult> roles()
+        {
+            var roles = await _roleManager.Roles.OrderBy(r => r.Name).ToListAsync();
+            return View(roles);
         }
     }
 }
