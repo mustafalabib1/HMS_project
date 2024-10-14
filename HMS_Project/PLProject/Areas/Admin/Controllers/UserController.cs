906dbb474890bd27545a55127c839d5c075f369f
Fares Mahmoud <yepiwater@outlook.com>
2024-10-14 17:11:54 +0300

    Admin Can Now Change other users' Roles

--- HMS_Project/PLProject/Areas/Admin/Controllers/UserController.cs
+++ HMS_Project/PLProject/Areas/Admin/Controllers/UserController.cs
@@ -21,6 +21,14 @@ namespace PLProject.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
+        #region Roles Views
+        public async Task<IActionResult> roles()
+        {
+            var roles = await _roleManager.Roles.OrderBy(r => r.Name).ToListAsync();
+            return View(roles);
+        }
+        #endregion
+
         public async Task<IActionResult> Index()
         {
             var users = await _userManager.Users.Select(user => new AppUserViewModel
@@ -30,15 +38,64 @@ namespace PLProject.Areas.Admin.Controllers
                 FullName = user.FullName,
                 Email = user.Email,
                 PhoneNumber = user.PhoneNumber ?? string.Empty,
-                Roles = _userManager.GetRolesAsync(user).Result
+                Roles = _userManager.GetRolesAsync(user).GetAwaiter().GetResult()
             }
                 ).ToListAsync();
+
+            users = users
+                .OrderByDescending(user => user.Roles.Contains("Admin"))
+                .ThenBy(user => user.UserName)
+                .ToList();
+
             return View(users);
         }
-        public async Task<IActionResult> roles()
+
+        public async Task<IActionResult> ManageRoles(string userId)
         {
-            var roles = await _roleManager.Roles.OrderBy(r => r.Name).ToListAsync();
-            return View(roles);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+                return NotFound();
+
+            var roles = await _roleManager.Roles.ToListAsync();
+
+            var viewModel = new UserRolesViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Roles = roles.Select(role => new RoleViewModel
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name,
+                    IsSelected = _userManager.IsInRoleAsync(user, role.Name).Result
+                }).ToList()
+            };
+
+            return View(viewModel);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRoles(UserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+
+            if (user == null)
+                return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in model.Roles)
+            {
+                if (userRoles.Any(r => r == role.RoleName) && !role.IsSelected)
+                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+
+                if (!userRoles.Any(r => r == role.RoleName) && role.IsSelected)
+                    await _userManager.AddToRoleAsync(user, role.RoleName);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
