fafd37044d9e09fac5294103a08099acba2a2f24
Fares Mahmoud <faressafi203@gmail.com>
2024-10-15 15:34:06 +0300

    added user repos

---
+++ HMS_Project/BLLProject/Interfaces/IUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using BLLProject.Specification;
+using DALProject.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLLProject.Interfaces
+{
+    public interface IUserRepository<T> where T : AppUser
+    {
+        public void Add(T entity);
+
+        public void Update(T entity);
+
+        public void Delete(T entity);
+
+        public T Get(string Id);
+
+        public IEnumerable<T> GetALL();
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> filter);
+
+        //public T GetEntityWithSpec(ISpecification<T> spec);
+        //public IEnumerable<T> GetALLWithSpec(ISpecification<T> spec);
+    }
+}
---
+++ HMS_Project/BLLProject/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using BLLProject.Interfaces;
+using BLLProject.Specification;
+using DALProject.Data.Contexts;
+using DALProject.model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLLProject.Repositories
+{
+    public class UserRepository<T> : IUserRepository<T> where T : AppUser
+    {
+        private readonly HMSdbcontext context;
+
+        public UserRepository(HMSdbcontext context)
+        {
+            this.context = context;
+        }
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+
+        }
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+
+        }
+        public void Update(T entity)
+        {
+            context.Set<T>().Update(entity);
+
+        }
+
+        public T Get(string Id) => context.Set<T>().Find(Id);
+
+        public IEnumerable<T> GetALL() => context.Set<T>().AsNoTracking().ToList();
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> filter)
+        {
+            return context.Set<T>().Where(filter);
+        }
+
+        public T GetEntityWithSpec(IUserSpecification<T> spec) => ApplySpec(spec).FirstOrDefault();
+
+        public IEnumerable<T> GetALLWithSpec(IUserSpecification<T> spec) => ApplySpec(spec).AsNoTracking().ToList();
+        //helper
+        private IQueryable<T> ApplySpec(IUserSpecification<T> spec) => UserSpecificationEvaluator<T>.GetQuery(context.Set<T>(), spec);
+    }
+}
